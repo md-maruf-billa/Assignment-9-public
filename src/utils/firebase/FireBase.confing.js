1fd9b3b91efe6db9f560c949f5447cb99132d2f1
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-04-12 17:19:08 +0600

    Added env file for encript firebase api keys

--- src/utils/firebase/FireBase.confing.js
+++ src/utils/firebase/FireBase.confing.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEp5cijSYaMcqb-EBOctrTyHI4Z5GnKyw",
-  authDomain: "citizen-central.firebaseapp.com",
-  projectId: "citizen-central",
-  storageBucket: "citizen-central.appspot.com",
-  messagingSenderId: "323276185825",
-  appId: "1:323276185825:web:5877e027165637f00697a0"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
